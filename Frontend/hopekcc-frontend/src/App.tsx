dba1683bda87e33c5df0a530f430ab2fa20f3738
lucasjia <lucasjia@ucsb.edu>
2024-07-21 21:09:55 -0700

    frontend test. update gitignore

--- Frontend/hopekcc-frontend/src/App.tsx
+++ Frontend/hopekcc-frontend/src/App.tsx
@@ -28,6 +28,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="bg-blue-500 text-white p-4">
+        Tailwind CSS is working!
+    </div>
+
     </>
   )
 }

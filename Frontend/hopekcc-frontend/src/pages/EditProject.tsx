f2c58626bda8834fc0edc91ef9977eff0b7208b8
thejackys <wasjacky1114@gmail.com>
2024-08-20 19:58:46 -0400

    add more Docs to apis.

--- Frontend/hopekcc-frontend/src/pages/EditProject.tsx
+++ Frontend/hopekcc-frontend/src/pages/EditProject.tsx
@@ -76,9 +76,13 @@ const ProjectEditor: React.FC = () => {
   
   //data for display
   const [activeFileID, setActiveFileID] = useState(fileIds[0] || 0);
-  let localFiles = useMemo(() => {
-    return fetchedFileContents;
+  const [localFiles, setLocalFiles] = useState<File[]>([]);
+  useEffect(() => {
+    setLocalFiles(fetchedFileContents);
   }, [fetchedFileContents]);
+  // let localFiles = useMemo(() => {
+  //   return fetchedFileContents;
+  // }, [fetchedFileContents]);
 
   const [isEditing, setIsEditing] = useState<boolean>(true);
   const [isCollapsedFileTab, setIsCollapsedFileTab] = useState<boolean>(false);

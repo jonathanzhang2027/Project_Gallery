331646a709f694194948eb47e14dfcba8fac41c1
joshualin1212 <93230081+joshualin1212@users.noreply.github.com>
2024-08-21 23:45:44 +0800

    minor bug: fixed projects not showing up

--- Frontend/hopekcc-frontend/src/pages/Home.tsx
+++ Frontend/hopekcc-frontend/src/pages/Home.tsx
@@ -2,49 +2,56 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { useQuery } from "react-query";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Clock, Calendar } from 'lucide-react';
+import { Clock, Calendar } from "lucide-react";
 import { Project } from "../utils/types.ts";
-import {Button, DeleteButton } from "../components/projectComponents/Buttons.tsx";
-import { useProjectOperations } from "../utils/api.ts"; 
+import {
+  Button,
+  DeleteButton,
+} from "../components/projectComponents/Buttons.tsx";
+import { useProjectOperations } from "../utils/api.ts";
 const ProjectList = ({ projects }: { projects: Project[] }) => {
   const ProjectHeader = () => {
     return (
       <div className="grid grid-cols-12 gap-4 items-center py-3 rounded-md transition-colors duration-150">
         <div className="col-span-3 text-left">Name</div>
         <div className="col-span-4 text-left">Description</div>
-        <div className="col-span-2 text-left flex">Last Updated <Clock size={14} className="m-1" /></div>
-        <div className="col-span-2 text-left flex ">Created <Calendar size={14} className="m-1 " /> </div>
-        <div className="col-span-1 text-left flex ">  </div>
+        <div className="col-span-2 text-left flex">
+          Last Updated <Clock size={14} className="m-1" />
+        </div>
+        <div className="col-span-2 text-left flex ">
+          Created <Calendar size={14} className="m-1 " />{" "}
+        </div>
+        <div className="col-span-1 text-left flex "> </div>
       </div>
     );
   };
-  const ProjectItem = ({project}: {project: Project}) => {
+  const ProjectItem = ({ project }: { project: Project }) => {
     const { handleProjectDelete } = useProjectOperations(project.id);
     return (
-    <div className="grid grid-cols-12 gap-4 items-center py-3 hover:bg-gray-50 rounded-md transition-colors duration-150">
-      <div className="col-span-3">
-        <h3 className="text-left font-medium text-blue-800 hover:underline truncate">
-          <Link to={`/projects/${project.id}`}>
-            {project.name}
-          </Link>
-        </h3>
+      <div className="grid grid-cols-12 gap-4 items-center py-3 hover:bg-gray-50 rounded-md transition-colors duration-150">
+        <div className="col-span-3">
+          <h3 className="text-left font-medium text-blue-800 hover:underline truncate">
+            <Link to={`/projects/${project.id}`}>{project.name}</Link>
+          </h3>
+        </div>
+        <div className="col-span-4 text-sm text-left text-gray-500 truncate">
+          {project.description}
+        </div>
+        <div className="col-span-2 flex items-center text-xs text-gray-400">
+          <span className="truncate">
+            {new Date(project.updated_at).toLocaleDateString()}
+          </span>
+        </div>
+        <div className="col-span-2 flex items-center text-xs text-gray-400">
+          <span className="truncate">
+            {new Date(project.created_at).toLocaleDateString()}
+          </span>
+        </div>
+        <DeleteButton onClick={handleProjectDelete} className="mx-2" />
       </div>
-      <div className="col-span-4 text-sm text-left text-gray-500 truncate">
-        {project.description}
-      </div>
-      <div className="col-span-2 flex items-center text-xs text-gray-400">
-  
-        <span className="truncate">{new Date(project.updated_at).toLocaleDateString()}</span>
-      </div>
-      <div className="col-span-2 flex items-center text-xs text-gray-400">
-        <span className="truncate">{new Date(project.created_at).toLocaleDateString()}</span>
-      </div>
-      <DeleteButton onClick={handleProjectDelete} className="mx-2"/>
-    </div>
-    )
-  }
-  
-  
+    );
+  };
+
   return (
     <div className="bg-white container mx-auto px-4 py-4">
       <div className="divide-y divide-gray-200">
@@ -54,7 +61,9 @@ const ProjectList = ({ projects }: { projects: Project[] }) => {
         {projects.length === 0 ? (
           <p className="text-gray-500 italic py-3">No projects available</p>
         ) : (
-          projects.map((project: Project) => <ProjectItem key={project.id} project={project} />)
+          projects.map((project: Project) => (
+            <ProjectItem key={project.id} project={project} />
+          ))
         )}
       </div>
     </div>
@@ -78,7 +87,7 @@ const Home = () => {
       },
     });
     console.log(response);
-    return response.data.projects;
+    return response.data;
   };
 
   const { data, isLoading, isError, error } = useQuery<Project[]>(
